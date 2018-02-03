0083692c2a036b1a8f959cd72aeedeb94d541880
Xu Qian <xuqianshawn@gmail.com>
2018-02-03 15:37:20 +0800

    implement Queue using two stacks

---
+++ LeetCode/ProgramMain.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    class ProgramMain
+    {
+        public static void Main(string[] args)
+        {
+            MyQueue<int> test = new MyQueue<int>();
+            test.Enqueue(1);
+            test.Enqueue(2);
+            test.Enqueue(3);
+            test.Enqueue(4);
+            test.Enqueue(5);
+            test.Dequeue();
+            test.Enqueue(6);
+            test.Enqueue(7);
+            Console.Out.WriteLine(test.Dequeue());
+            Console.Out.WriteLine(test.Dequeue());
+            Console.Out.WriteLine(test.Dequeue());
+            Console.Out.WriteLine(test.Dequeue());
+            Console.Out.WriteLine(test.Dequeue());
+            Console.Out.WriteLine(test.Dequeue());
+        }
+    }
+}
---
+++ LeetCode/Queue.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    class MyQueue<T>
+    {
+        private Stack<T> _stack1=new Stack<T>();
+        private Stack<T> _stack2=new Stack<T>();
+        public void Enqueue(T input)
+        {
+            _stack1.Push(input);
+        }
+        public T Dequeue()
+        {
+            while (_stack1.Count>0)
+            {
+                _stack2.Push(_stack1.Pop());
+            }
+            //return the oldest item
+            var temp = _stack2.Pop();
+
+            while (_stack2.Count()>0)
+            {
+                _stack1.Push(_stack2.Pop());
+            }
+            return temp;
+        }
+    }
+}
